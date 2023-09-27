d2ff72ad6f43ececda9579bc7f1e8cd4d6d631e3
nift4 <nift4@protonmail.com>
2023-09-27 21:02:34 +0200

    Launcher3: cheap fix for taskbar app icons disappearing in portrait
    This should be revisited later, just to find out why this is needed,
    but "if it works, it works" :D
    
    Change-Id: I7f981375ec31c46b061e0559c01cf02a2b0f81e9

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -613,7 +613,8 @@ public class DeviceProfile {
                 || inv.inlineQsb[INDEX_TWO_PANEL_LANDSCAPE]
                 : inv.inlineQsb[INDEX_DEFAULT] || inv.inlineQsb[INDEX_LANDSCAPE])
                 && hotseatQsbHeight > 0 && showQsb;
-        isQsbInline = isScalableGrid && inv.inlineQsb[mTypeIndex] && canQsbInline;
+        isQsbInline = (mIsScalableGrid || (isTaskbarPresent && !isLandscape))
+                && inv.inlineQsb[mTypeIndex] && canQsbInline;
 
         areNavButtonsInline = isTaskbarPresent && !isGestureMode;
         numShownHotseatIcons =
@@ -957,7 +958,7 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
-        if (!mIsScalableGrid) return;
+        if (!(mIsScalableGrid || (isTaskbarPresent && !isLandscape))) return;
 
         updateHotseatWidthAndBorderSpace(inv.numColumns);
         int numWorkspaceColumns = getPanelCount() * inv.numColumns;
