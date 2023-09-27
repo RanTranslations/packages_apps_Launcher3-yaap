450e0017458a7410e88aab4ef6503ebe96dc826f
nift4 <nift4@protonmail.com>
2023-09-27 20:00:53 +0200

    Launcher3: rewrite qsb
    1. Move away from fragments for taskbar support
    (Tested QSB setup flow by manually removing BIND_APPWIDGET permission)
    2. Remove useless logic
    3. Handle widget update in-view without launcher restart
    4. Add basic support for non-Google search app
    
    Change-Id: Id76f4e2092d4d49003c245602a2450b82b616a34

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -59,6 +59,7 @@ import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.qsb.QsbContainerView;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
@@ -353,7 +354,7 @@ public class SettingsActivity extends FragmentActivity
             }
 
             if (mShowGoogleBarPref != null) {
-                mShowGoogleBarPref.setEnabled(Utilities.isGSAEnabled(getContext()));
+                mShowGoogleBarPref.setEnabled(QsbContainerView.getSearchWidgetPackageName(getContext()) != null);
             }
         }
 
